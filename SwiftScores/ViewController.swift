e5be145f0c9082c5d02f2d7f8eb47a4b42981f36
Xiaoyu Hu <hufk@bc.edu>
2019-05-15 15:01:53 -0400

    Initial Commit

---
+++ SwiftScores/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftScores
+//
+//  Created by Xiaoyu Hu on 5/15/19.
+//  Copyright © 2019 Xiaoyu Hu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
